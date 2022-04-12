fcefede3cd39b9d4919bf437d96338da79e353bf
jbferraz <>
2022-04-12 10:43:56 -0300

    atualização no main.

--- src/src/main.java
+++ src/src/main.java
@@ -17,6 +17,7 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Prof. Jair Ferraz");
+        System.out.println("Senac Tech");
     }
     
 }
